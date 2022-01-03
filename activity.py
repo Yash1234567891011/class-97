4ab7ada263849f06b5618f04c75fbdd7b21a80f8
Yash1234567891011 <73171838+Yash1234567891011@users.noreply.github.com>
2022-01-03 16:51:00 +0530

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,9 @@
+intro=input("Enter the string: ")
+cc=0
+wc=1
+for i in intro:
+    cc=cc+1
+    if i==" ":
+        wc=wc+1
+print("number of words in the string: ",wc)  
+print("number of character in the string: ",cc)       
